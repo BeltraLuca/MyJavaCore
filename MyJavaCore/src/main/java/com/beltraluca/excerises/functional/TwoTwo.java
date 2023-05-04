b4ac848789bc11a967608bded845bf69e60a94dd
Beltraluca <lucabeltrami2002@hotmail.com>
2023-05-04 12:23:29 +0200

    adding

--- MyJavaCore/src/main/java/com/beltraluca/excerises/functional/TwoTwo.java
+++ MyJavaCore/src/main/java/com/beltraluca/excerises/functional/TwoTwo.java
@@ -3,7 +3,7 @@ package com.beltraluca.excerises.functional;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class TwoTen {
+public class TwoTwo {
 
     public static List<Integer> twoTwo(List<Integer> nums){
         return nums.stream().map(integer -> integer*2).filter(integer -> integer%10!=2).collect(Collectors.toList());
--- MyJavaCore/src/test/java/com/beltraluca/excerises/functional/TwoTwoTest.java
+++ MyJavaCore/src/test/java/com/beltraluca/excerises/functional/TwoTwoTest.java
@@ -5,13 +5,11 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-class TwoTenTest {
+class TwoTwoTest {
 
     @Test
     void twoTwo() {
         List<Integer> l=new ArrayList<>(List.of(1, 2, 3));
-        System.out.println(TwoTen.twoTwo(l).toString());
+        System.out.println(TwoTwo.twoTwo(l).toString());
     }
 }
